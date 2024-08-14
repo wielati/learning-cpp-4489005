40e0aba495ad74daf8eef3eca3ea5c12aea6f574
wielati <wielati1@alcon.net>
2024-08-14 14:58:52 +0000

    first comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,6 +1,7 @@
 // Learning C++ 
 // Exercise 00_03
-// Using the exercise files on GitHub, by Eduardo Corpeño 
+// Using the exercise files on GitHub, by Eduardo Corpeño
+// tiw version
 
 #include <iostream>
 
